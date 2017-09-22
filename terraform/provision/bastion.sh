b3a88d785a6de3ea374ea090a1bab1c0f1473445
alejandronb <ale90nb@gmail.com>
2017-09-22 10:25:56 +0200

    Comment yum update

--- terraform/provision/bastion.sh
+++ terraform/provision/bastion.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo yum -y install epel-release
-sudo yum -y update
+# Deshabilitar temporalmente update debido al tiempo que necesita
+# sudo yum -y update
 sudo yum -y install ansible pyOpenSSL wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
